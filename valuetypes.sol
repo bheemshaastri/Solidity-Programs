1343a670fd42c582e41dda03f468c87f992cfd29
Jangam Bheemeswara Sastry <103921125+bheemshaastri@users.noreply.github.com>
2022-05-04 13:16:06 +0530

    Value Types.sol

---
+++ valuetypes.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.13; 
+
+contract ValueTypes 
+
+{ 
+    
+    bool public Myboolean_Variable=true; 
+    int public MyInteger_Variable=-123; 
+    uint public MyUint_Variable=123; 
+    address public MyPublic_Address=0xfC9Dfbb7A367cdd20e8F064c857d1B138Ebf9FC7;
+    bytes32 public Myhash = 0xcedb8aa19dcf2ba82a4bbbbb946792d9804ac19196c7c3cab1ab110a51084aaf; 
+    
+}
