16cabe5cf54bb4ea5f92af468a211a5ca80896b6
Jangam Bheemeswara Sastry <103921125+bheemshaastri@users.noreply.github.com>
2022-05-04 13:14:52 +0530

    greetings.sol

---
+++ greetings.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.13;
+
+contract HelloWorld 
+{ 
+    string public Mygreetings = "Hi Students, Welcome to Solidity Programming Language"; 
+}
